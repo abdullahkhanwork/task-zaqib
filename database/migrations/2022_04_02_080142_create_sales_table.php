c7df841313bc99288ac0ace10c86ffa5b836174c
Abdullah Khan <abdullahkhanwork@gmail.com>
2022-04-02 12:20:59 +0500

    migrations

---
+++ database/migrations/2022_04_02_080142_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('customer_id');
+            $table->index('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customer')->onDelete('cascade');
+            $table->unsignedInteger('product_id');
+            $table->index('product_id');
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+};
---
+++ database/migrations/2022_04_02_080202_create_sale_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('sale_id');
+            $table->index('sale_id');
+            $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+            $table->integer('qty');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_items');
+    }
+};
